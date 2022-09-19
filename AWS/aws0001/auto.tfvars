d2ba379762cece139abe8e65a1d777dc61479a73
Carlos Castillo <carlos@castillo-a.com>
2022-09-18 23:11:37 -0500

    Update tags

--- AWS/aws0001/auto.tfvars
+++
@@ -1,1 +0,0 @@
-project = "aws0001"
--- AWS/aws0001/main.tf
+++ AWS/aws0001/main.tf
@@ -21,9 +21,12 @@ provider "aws" {
   region        = "us-east-1"
   access_key    = var.AWS_ACCESS_KEY
   secret_key    = var.AWS_SECRET_KEY
-  environment   = var.environment
+
   default_tags {
-    Stakeholder = "admin01@castillo-a.com"
-    IaC         = "terraform"
+    tags {
+      Stakeholder = "admin01@castillo-a.com"
+      IaC         = "terraform"
+      Project     = var.project
+    }
   }
 }
