8c7677dddc9d86850537fd31fa822c9e7754bdb5
Carlos Castillo <carlos@castillo-a.com>
2022-09-18 23:18:54 -0500

    S3 Bucket

--- AWS/aws0001/s3.tf
+++ AWS/aws0001/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "aws0001-bucket" {
   bucket = "${var.project}bucket"
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "aws0001-bucket-acl" {
   bucket = aws_s3_bucket.aws0001-bucket.id
   acl    = var.s3_acl
 }
