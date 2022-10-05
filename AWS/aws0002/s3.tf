475e689046619d5a5b0bb5e0c09dac5bd227ca61
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 18:46:44 -0500

    aws0002 Final update

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,8 +1,9 @@
 # Create bucket(s) via module
-module "s3_module" {
-  source      = "git::https://github.com/carlos-castillo-a/s3-module.git"
+module "s3-module" {
+  source  = "app.terraform.io/aws-castillo-a/s3-module/aws"
+  version = "1.0.1"
   project     = var.project
   environment = var.environment
   num         = 2
-  name_suffix = "test"
+  name_suffix = "final"
 }
