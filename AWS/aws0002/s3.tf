d8e80b2cf13a3b81877098045b8d1a42cf049e1a
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 22:38:03 -0500

    test multi-bucket creation

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,7 +1,8 @@
 # Create bucket(s) via module
 module "s3-module" {
-  source  = "app.terraform.io/aws-castillo-a/s3-module/aws"
-  version = "1.1.2"
+  # source      = "app.terraform.io/aws-castillo-a/s3-module/aws"
+  source      = "git::https://github.com/carlos-castillo-a/terraform-aws-s3-module.git"
+  # version     = "1.1.2"
   project     = var.project
   environment = var.environment
   num         = 2
