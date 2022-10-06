1219f692e8c95e33f35bb6439de9dd004bc8db2c
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 20:26:00 -0500

    Update to s3-module v1.1.2

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,7 +1,7 @@
 # Create bucket(s) via module
 module "s3-module" {
   source  = "app.terraform.io/aws-castillo-a/s3-module/aws"
-  version = "1.1.1"
+  version = "1.1.2"
   project     = var.project
   environment = var.environment
   num         = 2
