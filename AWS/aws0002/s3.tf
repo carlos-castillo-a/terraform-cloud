13ca0ae5f47d424f878e7fac417339c74eaba7bb
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 20:06:12 -0500

    Update source URL

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,6 +1,6 @@
 # Create bucket(s) via module
 module "s3-module" {
-  source  = "git::https://github.com/carlos-castillo-a/s3-module.git?ref=v1.1.0"
+  source  = "git::https://github.com/carlos-castillo-a/s3-module.git"
   # version = "1.0.3"
   project     = var.project
   environment = var.environment
