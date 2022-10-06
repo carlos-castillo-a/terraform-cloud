d8f39ee8db4423c5b756e22ec113a037d77ac13c
Carlos Castillo <carlos@castillo-a.com>
2022-10-05 19:55:12 -0500

    Test v1.1.0

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,7 +1,7 @@
 # Create bucket(s) via module
 module "s3-module" {
-  source  = "app.terraform.io/aws-castillo-a/s3-module/aws"
-  version = "1.0.3"
+  source  = "git::https://github.com/carlos-castillo-a/s3-module.git?ref=v1.1.0"
+  # version = "1.0.3"
   project     = var.project
   environment = var.environment
   num         = 2
--- AWS/aws0002/terraform.auto.tfvars
+++ AWS/aws0002/terraform.auto.tfvars
@@ -1,2 +1,9 @@
 # Project
 project = "aws0002"
+
+# Terraform Cloud Variables
+# AWS_ACCESS_KEY = {}
+# AWS_SECRET_KEY = {}
+# environment    = {}
+# organization   = {}
+# stakeholder    = {}
