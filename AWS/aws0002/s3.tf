a129678aaa68481b2b84fee491b594112275b581
Carlos Castillo <carlos@castillo-a.com>
2022-10-07 18:56:48 -0500

    Test versioning

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,7 +1,8 @@
 # Create bucket(s) via module
 module "s3-module" {
-  source             = "app.terraform.io/aws-castillo-a/s3-module/aws"
-  version            = "1.0.1"
+  # source             = "app.terraform.io/aws-castillo-a/s3-module/aws"
+  # version            = "1.0.1"
+  source             = "git::https://github.com/carlos-castillo-a/terraform-aws-s3-module.git"
   project            = var.project
   environment        = var.environment
   num                = 2
