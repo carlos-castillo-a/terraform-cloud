50d30bd86e094e974a67a37a272a054d7a591c25
Carlos Castillo <carlos@castillo-a.com>
2022-10-07 19:24:44 -0500

    Fix versioning attempt 08

--- AWS/aws0002/s3.tf
+++ AWS/aws0002/s3.tf
@@ -1,14 +1,14 @@
 # Create bucket(s) via module
 module "s3-module" {
-  # source            = "app.terraform.io/aws-castillo-a/s3-module/aws"                            # Required
-  # version           = "1.0.1"                                                                    # Required
+  # source            = "app.terraform.io/aws-castillo-a/s3-module/aws"                             # Required
+  # version           = "1.0.1"                                                                     # Required
   source              = "git::https://github.com/carlos-castillo-a/terraform-aws-s3-module.git"    # Required
   project             = var.project                                                                # Required
   environment         = var.environment                                                            # Required
   num                 = 2                                                                          # Required
 
-  name_suffix         = "test"                                                                     # Optional
-  block_public_access = false                                                                      # Optional
-  versioning_enabled  = true                                                                       # Optional
-  # tags              = {}                                                                         # Optional       
+  name_suffix         = "test"                                                                      # Optional
+  # block_public_access = true                                                                      # Optional
+  # versioning_enabled  = true                                                                      # Optional
+  # tags              = {}                                                                          # Optional       
 }
