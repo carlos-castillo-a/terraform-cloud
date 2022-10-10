fe2074b5354f9d5e18d59c869aba48e843cbcd73
Carlos Castillo <carlos@castillo-a.com>
2022-10-09 22:30:39 -0500

    Add features to azurerm

--- Azure/az0001/main.tf
+++ Azure/az0001/main.tf
@@ -23,7 +23,7 @@ terraform {
 
 # Configure the Azure Providers
 provider "azurerm" { 
-    
+  features {}
   subscription_id = var.ARM_SUBSCRIPTION_ID
   client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
